927f8b43ecc76139b55c5e617ce4a26b4c04f301
Justin Kreller <justin.kreller@stud.htwk-leipzig.de>
2023-01-08 00:59:44 +0100

    WIP start working on billing fragment

---
+++ app/src/main/java/com/github/flatit/ui/billing/BillingDeptViewModel.kt
@@ -0,0 +1,15 @@
+package com.github.flatit.ui.billing
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.github.flatit.data.model.BillingDeptItem
+
+class BillingDeptViewModel: ViewModel() {
+    val fakeData = listOf(
+        BillingDeptItem(flatMate = "Johannes", dept = 34.56),
+        BillingDeptItem(flatMate = "Moritz", dept = -56.8),
+        BillingDeptItem(flatMate = "Justin", dept = 65.56)
+    )
+
+    val items: MutableLiveData<List<BillingDeptItem>> = MutableLiveData(fakeData)
+}
--- app/src/main/java/com/github/flatit/ui/billing/BillingFragment.kt
+++ app/src/main/java/com/github/flatit/ui/billing/BillingFragment.kt
@@ -5,9 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.flatit.databinding.FragmentBillingBinding
 
 class BillingFragment : Fragment() {
+
+    private val billingExpenseViewModel by viewModels<BillingExpenseViewModel> ()
+    private val billingDeptViewModel by viewModels<BillingDeptViewModel> ()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,7 +20,25 @@ class BillingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentBillingBinding.inflate(inflater, container, false)
+        val root: View = binding.root
 
-        return binding.root
+        val expenseLayoutManager = LinearLayoutManager(context)
+        val deptLayoutManager = LinearLayoutManager(context)
+        val expenseAdapter = BillingExpenseAdapter()
+        val deptAdapter = BillingDeptAdapter()
+
+        binding.depts.adapter = deptAdapter
+        binding.expenses.adapter = expenseAdapter
+        binding.depts.layoutManager = deptLayoutManager
+        binding.expenses.layoutManager = expenseLayoutManager
+
+        billingExpenseViewModel.items.observe(viewLifecycleOwner) {
+            expenseAdapter.submitList(it)
+        }
+        billingDeptViewModel.items.observe(viewLifecycleOwner) {
+            deptAdapter.submitList(it)
+        }
+
+        return root
     }
 }
