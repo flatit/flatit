337e6675d0427bcb838503d88d828800a1f72d2b
jojomee <jojomee@users.noreply.github.com>
2023-01-08 18:05:42 +0100

    fix: keep selected fragment open when rotating device

--- app/src/main/java/com/github/flatit/MainActivity.kt
+++ app/src/main/java/com/github/flatit/MainActivity.kt
@@ -2,6 +2,9 @@ package com.github.flatit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import androidx.fragment.app.Fragment
 import com.github.flatit.ui.billing.BillingFragment
 import com.github.flatit.ui.overview.OverviewFragment
 import com.github.flatit.ui.shoppinglist.ShoppingListFragment
@@ -15,29 +18,38 @@ class MainActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction().replace(R.id.container, OverviewFragment()).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.container, getFragment(savedInstanceState?.getInt("selectedFragment"))).commit()
 
         val navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         navigation.setOnItemSelectedListener { item ->
-            when(item.itemId) {
-                R.id.page_overview -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.container, OverviewFragment()).commit()
-                    true
-                }
-                R.id.page_shopping_list -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.container, ShoppingListFragment()).commit()
-                    true
-                }
-                R.id.page_billing -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.container, BillingFragment()).commit()
-                    true
-                }
-                R.id.page_todos -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.container, TodosFragment()).commit()
-                    true
-                }
-                else -> false
+            supportFragmentManager.beginTransaction().replace(R.id.container, getFragment(item.itemId)).commit()
+
+            true
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        val navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        outState.putInt("selectedFragment", navigation.selectedItemId)
+    }
+
+    private fun getFragment(id: Int?) : Fragment {
+        return when(id) {
+            R.id.page_overview -> {
+                OverviewFragment()
             }
+            R.id.page_shopping_list -> {
+                ShoppingListFragment()
+            }
+            R.id.page_billing -> {
+                BillingFragment()
+            }
+            R.id.page_todos -> {
+                TodosFragment()
+            }
+            else -> OverviewFragment()
         }
     }
 }
