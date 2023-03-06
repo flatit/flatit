f333915ccea28682fb81042662a1c83ec9956938
jojomee <6502589+JojoMee@users.noreply.github.com>
2023-03-06 18:04:17 +0100

    feat: add cards to overview fragment that lead to the corresponding page

--- app/src/main/java/com/github/flatit/ui/overview/OverviewFragment.kt
+++ app/src/main/java/com/github/flatit/ui/overview/OverviewFragment.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.github.flatit.R
 import com.github.flatit.databinding.FragmentOverviewBinding
 
-class OverviewFragment : Fragment() {
+class OverviewFragment(
+    private val selectFragment: (id: Int?) -> Unit
+) : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +18,10 @@ class OverviewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentOverviewBinding.inflate(inflater, container, false)
+
+        binding.overviewCardShopping.setOnClickListener { selectFragment(R.id.page_shopping_list) }
+        binding.overviewCardFinances.setOnClickListener { selectFragment(R.id.page_finances) }
+        binding.overviewCardTodos.setOnClickListener { selectFragment(R.id.page_todos) }
 
         return binding.root
     }
